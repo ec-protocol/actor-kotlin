1a10e6eef8ae73ba2e8fd7f0c16053a277518651
Timon Schelling <timon.schelling@outlook.de>
2020-09-16 11:21:23 +0200

    -

--- src/ec/actor/Connection.kt
+++ src/ec/actor/Connection.kt
@@ -4,11 +4,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.selects.whileSelect
 import javax.security.auth.kerberos.EncryptionKey
 
 class Connection(
-        private val i: Channel<ByteArray>,
-        private val o: Channel<ByteArray>,
+        private val rawInputChannel: Channel<ByteArray>,
+        private val rawOutputChannel: Channel<ByteArray>,
         private val scope: CoroutineScope = GlobalScope
 ) {
 
@@ -24,17 +25,45 @@ class Connection(
 
     fun init(encrypt: Boolean = true) {
         if (!encrypt) {
-            scope.launch {
-                handleInput()
-            }
-            scope.launch {
-                handleOutput()
-            }
+            startHandelInput()
+            startHandelOutput()
+        }
+    }
+
+    private fun startHandelInput() {
+        scope.launch {
+            handleInput()
+        }
+    }
+
+    private fun startHandelOutput() {
+        scope.launch {
+            handleOutput()
         }
     }
 
     suspend fun handleInput() {
-
+        val encrypt = inputSessionKey.isNotEmpty() && outputSessionKey.isNotEmpty()
+        var state = 0
+        var currentPackageChannel = Channel<ByteArray>()
+        var currentControlPackageChannel = Channel<ByteArray>()
+        var leftover = byteArrayOf()
+        whileSelect {
+            resetInput.onReceive {
+                startHandelInput()
+                false
+            }
+            cancelInput.onReceive {
+                false
+            }
+            rawInputChannel.onReceive {
+                var remaining = leftover + it
+                leftover = byteArrayOf()
+                //TODO add encryption
+                TODO()
+                true
+            }
+        }
     }
 
     suspend fun handleOutput() {
---
+++ src/ec/actor/ControlByte.kt
@@ -0,0 +1,9 @@
+package ec.actor
+
+enum class ControlByte {
+    PACKAGE_START,
+    PACKAGE_END,
+    CONTROL_PACKAGE_START,
+    CONTROL_PACKAGE_END,
+    IGNORE
+}
