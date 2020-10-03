ebe7115db2e278941e79c2484a790ccebadf95ed
Timon Schelling <timon.schelling@outlook.de>
2020-10-03 18:20:12 +0200

    -

--- src/Main.kt
+++ src/Main.kt
@@ -4,12 +4,12 @@ import ec.actor.Connection
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
 
-suspend fun main() {
+fun main() = runBlocking {
     var isClient = true
     val socket = try {
         aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect("localhost", 6542)
@@ -21,7 +21,7 @@ suspend fun main() {
     val readChannel = Channel<ByteArray>()
     val writeChannel = Channel<ByteArray>()
 
-    GlobalScope.launch {
+    launch {
         val channel = socket.openReadChannel()
         while (true) {
             var buffer = byteArrayOf()
@@ -33,7 +33,7 @@ suspend fun main() {
         }
     }
 
-    GlobalScope.launch {
+    launch {
         val channel = socket.openWriteChannel(true)
         while (true) {
             val buf = writeChannel.receive()
@@ -45,24 +45,29 @@ suspend fun main() {
     connection.init(false)
 
     if (isClient) {
+        val inBytes = File("data/in.mp4").readBytes()
         val pkgInputChannel = connection.input.receive()
         var pkg = byteArrayOf()
         while (true) {
             val it = pkgInputChannel.receive() ?: break
             pkg += it
         }
+        val recived = unescape(pkg)
         File("data/out.mp4").apply {
             if (exists()) return@apply
             parentFile?.mkdirs()
             createNewFile()
-        }.writeBytes(unescape(pkg))
-        print("done!!!")
+        }.writeBytes(recived)
+        if(inBytes contentEquals recived) {
+            println("complete")
+        } else {
+            println("failed")
+        }
     } else {
         val channel = Channel<ByteArray?>()
         connection.output.send(channel)
         val element = escape(File("data/in.mp4").readBytes())
         channel.send(element)
         channel.send(null)
-        print("done!!!")
     }
 }
